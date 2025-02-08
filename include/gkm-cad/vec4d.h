4e6e8ce3945a06fc4cc481cc60c4013ab1753a63
Petr Petrovich Petrov <petr@gkmsoft.ru>
2025-02-08 22:53:41 +0300

    Development has been started

---
+++ include/gkm-cad/vec4d.h
@@ -0,0 +1,4 @@
+// Copyright 2025 Petr Petrov. All rights reserved.
+// License: https://github.com/PetrPPetrov/gkm-local/blob/main/LICENSE
+
+#pragma once
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+// Copyright 2025 Petr Petrov. All rights reserved.
+// License: https://github.com/PetrPPetrov/gkm-local/blob/main/LICENSE
+
+#include <cstddef>
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, GKM-CAD!" << std::endl;
+}
